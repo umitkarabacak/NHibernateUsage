01afdbd59cfc2a5401d4092ec2001bc5667d078a
Ümit KARABACAK <umit.karabacak@hotmail.com.tr>
2024-09-02 22:07:16 +0300

    create empty project

---
+++ NHibernateUsage.WebApi/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿namespace NHibernateUsage.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        await Task.CompletedTask;
+
+        return Ok();
+    }
+}
