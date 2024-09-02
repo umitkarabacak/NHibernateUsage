0a5d11eddf8e0890657e324fc08ef66069e98dd2
Ümit KARABACAK <umit.karabacak@hotmail.com.tr>
2024-09-02 22:12:17 +0300

    create CrdCardMiscAuthProfileDef & CrdCardMiscAuthProfileDet entities

---
+++ NHibernateUsage.WebApi/Entities/CrdCardMiscAuthProfileDet.cs
@@ -0,0 +1,8 @@
+﻿namespace NHibernateUsage.WebApi.Entities;
+
+public class CrdCardMiscAuthProfileDet
+{
+    public virtual Guid CardMiscAuthProfileGuid { get; set; }
+    public virtual decimal FallbackAmount { get; set; }
+    public virtual CrdCardMiscAuthProfileDef CrdCardMiscAuthProfileDef { get; set; }
+}
